2ade9dea55b8b44b4d3275469580f88d4a7c68e9
Zeroxx <franciscopa91@gmail.com>
2017-07-13 00:02:36 +0200

    Detail Cycle Complete

--- src/containers/book_detail.js
+++ src/containers/book_detail.js
@@ -5,42 +5,35 @@ import { bindActionCreators } from 'redux';
 
 class BookDetail extends Component {
 
-    renderList(){
-        return this.props.books.map((book) => {
-            return (
-                <li
-                    key={book.title}
-                    onClick={() => this.props.selectBook(book)}
-                    className="list-group-item">
-                    {book.title}
-                </li>
-            )
-        })
-    }
-
     
     render(){
+        
+        if(!this.props.book){
+            return <div>Select a book to get started</div>;
+        }
+
         return(
-            <div>BookDetail</div>
+
+            <div>
+                <h3>Details for:</h3>
+                <div>{this.props.book.title}</div>
+            </div>
         )
     }
 }
 
 function mapStateToProps(state){
-    //Whenever is returned will show up as props
-    //inside of the Booklist
+
     return {
-        books: state.books
+        book: state.activeBook
     }
 }
-//Anything return wil end ups as props on the BookList container
+
+
 function mapDispatchToProps(dispatch){
-    //Whenever selectBook it's called, the result should be passed
-    //to all of our reducers
+
     return bindActionCreators({ selectBook : selectBook }, dispatch);
 }
 
-//Promote BookList from a component to a container -it needs to know
-//about this new dispatch method, selectBook. Make available
-//as a prop
-export default connect(mapStateToProps, mapDispatchToProps)(BookDetail);
+
+export default connect(mapStateToProps)(BookDetail);
