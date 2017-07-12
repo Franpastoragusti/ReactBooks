1625283c9b0b3cef7757944933e1e7e598168a72
Zeroxx <franciscopa91@gmail.com>
2017-07-13 00:05:06 +0200

    Added pages to books

--- src/containers/book_detail.js
+++ src/containers/book_detail.js
@@ -7,7 +7,7 @@ class BookDetail extends Component {
 
     
     render(){
-        
+
         if(!this.props.book){
             return <div>Select a book to get started</div>;
         }
@@ -16,7 +16,8 @@ class BookDetail extends Component {
 
             <div>
                 <h3>Details for:</h3>
-                <div>{this.props.book.title}</div>
+                <div>Title: {this.props.book.title}</div>
+                <div>Pages: {this.props.book.pages}</div>
             </div>
         )
     }
--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,20 @@
 export default () => {
     return [
-        {title:'Javascript: The good parts'},
-        {title:'Harry Potter'},
-        {title:'The Dark Tower'},
-        {title:'Eloquent Ruby'}
+        {
+            title:'Javascript: The good parts',
+            pages: 134
+        },
+        {
+            title:'Harry Potter',
+            pages: 231
+        },
+        {
+            title:'The Dark Tower',
+            pages: 453
+        },
+        {
+            title:'Eloquent Ruby',
+            pages: 412
+        }
     ]
 }
